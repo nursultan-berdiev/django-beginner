861f408a2130c10686d6c86aec5a371ec2ef3ea4
Nursultan Berdiev <dominguez120205024@gmail.com>
2023-03-09 21:02:10 +0600

    Changed admin.py in polls app

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text', 'pub_date']
+
+
+admin.site.register(Question, QuestionAdmin)
 
 
 @admin.register(Choice)
